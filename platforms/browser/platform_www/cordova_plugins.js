2d1c7c8e73b822e8d609d0acac8452fbce14940b
Reginald Sapp <reginaldsapp@Reginalds-MBP.home>
2018-11-07 21:34:38 -0500

    Add existing file

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-streaming-media-xtreme/www/StreamingMedia.js",
+        "id": "cordova-plugin-streaming-media-xtreme.StreamingMedia",
+        "pluginId": "cordova-plugin-streaming-media-xtreme",
+        "clobbers": [
+            "streamingMedia"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-streaming-media-xtreme": "2.3.1"
+}
+// BOTTOM OF METADATA
+});
